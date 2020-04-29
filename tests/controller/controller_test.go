16c74a3195350f2bbedaab6bc7d45a6bc3999fba
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-04-29 02:03:48 -0300

    Adiciona testes de controller

---
+++ tests/controller/controller_test.go
@@ -0,0 +1,108 @@
+package controller
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"github.com/matancredi/superheroes-api/api/controllers"
+	"github.com/matancredi/superheroes-api/api/models"
+)
+
+var server = controllers.Server{}
+var superInstance = models.Super{}
+
+// Configures .env and database
+func TestMain(m *testing.M) {
+	err := godotenv.Load(os.ExpandEnv("../../.env"))
+	if err != nil {
+		log.Fatalf("Error getting env %v\n", err)
+	}
+	Database()
+
+	os.Exit(m.Run())
+
+}
+
+func Database() {
+
+	var err error
+
+	TestDbDriver := os.Getenv("TestDbDriver")
+
+	if TestDbDriver == "postgres" {
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("TestDbHost"), os.Getenv("TestDbPort"), os.Getenv("TestDbUser"), os.Getenv("TestDbName"), os.Getenv("TestDbPassword"))
+		server.DB, err = gorm.Open(TestDbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database\n", TestDbDriver)
+			log.Fatal("This is the error:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database\n", TestDbDriver)
+		}
+	}
+}
+
+// Resets database
+func refreshSuperTable() error {
+
+	err := server.DB.DropTableIfExists(&models.Super{}).Error
+	if err != nil {
+		return err
+	}
+	err = server.DB.AutoMigrate(&models.Super{}).Error
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Successfully refreshed table")
+	return nil
+}
+
+// Seeds one super
+func seedOneSuper() (models.Super, error) {
+
+	err := refreshSuperTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	super := models.Super{
+		Name:      "Homem Aranha",
+		Biography: models.Biography{Alignment: "good"},
+	}
+
+	err = server.DB.Model(&models.Super{}).Create(&super).Error
+	if err != nil {
+		return models.Super{}, err
+	}
+	return super, nil
+}
+
+// Seeds more than one super
+func seedSupers() ([]models.Super, error) {
+
+	var err error
+	if err != nil {
+		return nil, err
+	}
+	supers := []models.Super{
+		models.Super{
+			Name:      "Pantera Negra",
+			Biography: models.Biography{Alignment: "bad"},
+		},
+		models.Super{
+			Name:      "Capitão América",
+			Biography: models.Biography{Alignment: "good"},
+		},
+	}
+	for i, _ := range supers {
+		err := server.DB.Model(&models.Super{}).Create(&supers[i]).Error
+		if err != nil {
+			return []models.Super{}, err
+		}
+	}
+	return supers, nil
+}
---
+++ tests/controller/super_controller_test.go
@@ -0,0 +1,246 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/matancredi/superheroes-api/api/models"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+// This test is not working because there is a conflict between .envs (CreateSuper is loading .env again)
+// func TestCreateSuper(t *testing.T) {
+
+// 	err := refreshSuperTable()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	samples := []struct {
+// 		inputJSON    string
+// 		statusCode   int
+// 		errorMessage string
+// 	}{
+// 		{
+// 			inputJSON: `{
+// 				name:      "Homem Aranha"
+// 			}`,
+// 			statusCode:   201,
+// 			errorMessage: "",
+// 		},
+// 		{
+// 			inputJSON: `{
+// 				name:      "Homem Aranha"
+
+// 			}`,
+// 			statusCode:   500,
+// 			errorMessage: "Name Already Taken",
+// 		},
+// 	}
+
+// 	for _, v := range samples {
+
+// 		req, err := http.NewRequest("POST", "/supers", bytes.NewBufferString(v.inputJSON))
+// 		if err != nil {
+// 			t.Errorf("this is the error: %v", err)
+// 		}
+// 		rr := httptest.NewRecorder()
+// 		handler := http.HandlerFunc(server.CreateSuper)
+// 		handler.ServeHTTP(rr, req))
+
+// 		super := models.Super{}
+// 		err = json.Unmarshal([]byte(rr.Body.String()), &super)
+// 		if err != nil {
+// 			fmt.Printf("Cannot convert to json: %v", err)
+// 		}
+// 		assert.Equal(t, rr.Code, v.statusCode)
+// 		if v.statusCode == 422 || v.statusCode == 500 && v.errorMessage != "" {
+// 			//assert.Equal(t, responseMap["error"], v.errorMessage)
+// 		}
+// 	}
+// }
+
+func TestGetSupers(t *testing.T) {
+
+	err := refreshSuperTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = seedSupers()
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest("GET", "/supers", nil)
+	if err != nil {
+		t.Errorf("this is the error: %v\n", err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(server.GetSupers)
+	handler.ServeHTTP(rr, req)
+
+	var supers []models.Super
+	err = json.Unmarshal([]byte(rr.Body.String()), &supers)
+	if err != nil {
+		log.Fatalf("Cannot convert to json: %v\n", err)
+	}
+	assert.Equal(t, rr.Code, http.StatusOK)
+
+	// There are two supers registered, so it has to return 2
+	assert.Equal(t, len(supers), 2)
+}
+
+func TestGetSuperByID(t *testing.T) {
+
+	err := refreshSuperTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	super, err := seedOneSuper()
+	if err != nil {
+		log.Fatal(err)
+	}
+	superSample := []struct {
+		uuid         string
+		statusCode   int
+		name         string
+		alignment    string
+		errorMessage string
+	}{
+		{
+			uuid:       strconv.Itoa(int(super.Uuid)),
+			alignment:  super.Biography.Alignment,
+			statusCode: 200,
+			name:       super.Name,
+		},
+		{
+			uuid:       "588",
+			statusCode: 500,
+		},
+	}
+	for _, v := range superSample {
+
+		req, err := http.NewRequest("GET", "/supers", nil)
+		if err != nil {
+			t.Errorf("This is the error: %v\n", err)
+		}
+		req = mux.SetURLVars(req, map[string]string{"uuid": v.uuid})
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.GetSuperById)
+		handler.ServeHTTP(rr, req)
+
+		newSuper := models.Super{}
+		err = json.Unmarshal([]byte(rr.Body.String()), &newSuper)
+		if err != nil {
+			log.Fatalf("Cannot convert to json: %v", err)
+		}
+
+		assert.Equal(t, rr.Code, v.statusCode)
+
+		if v.statusCode == 200 {
+			assert.Equal(t, super.Name, newSuper.Name)
+			assert.Equal(t, super.Biography.Alignment, newSuper.Biography.Alignment)
+		}
+	}
+}
+
+func TestGetSuperByName(t *testing.T) {
+
+	err := refreshSuperTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	super, err := seedOneSuper()
+	if err != nil {
+		log.Fatal(err)
+	}
+	superSample := []struct {
+		uuid         string
+		statusCode   int
+		name         string
+		alignment    string
+		errorMessage string
+	}{
+		{
+			uuid:       strconv.Itoa(int(super.Uuid)),
+			alignment:  super.Biography.Alignment,
+			statusCode: 200,
+			name:       super.Name,
+		},
+		{
+			name:       "banana",
+			statusCode: 500,
+		},
+	}
+	for _, v := range superSample {
+
+		req, err := http.NewRequest("GET", "/supers/search", nil)
+		if err != nil {
+			t.Errorf("This is the error: %v\n", err)
+		}
+		req = mux.SetURLVars(req, map[string]string{"name": v.name})
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.GetSuperByName)
+		handler.ServeHTTP(rr, req)
+
+		newSuper := models.Super{}
+		err = json.Unmarshal([]byte(rr.Body.String()), &newSuper)
+		if err != nil {
+			log.Fatalf("Cannot convert to json: %v", err)
+		}
+
+		assert.Equal(t, rr.Code, v.statusCode)
+
+		if v.statusCode == 200 {
+			assert.Equal(t, super.Uuid, newSuper.Uuid)
+			assert.Equal(t, super.Biography.Alignment, newSuper.Biography.Alignment)
+		}
+	}
+}
+
+func TestDeleteSuper(t *testing.T) {
+
+	err := refreshSuperTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	seedOneSuper()
+
+	superSample := []struct {
+		uuid         string
+		statusCode   int
+		errorMessage string
+	}{
+		{
+			uuid:         "1",
+			statusCode:   200,
+			errorMessage: "",
+		},
+	}
+	for _, v := range superSample {
+
+		req, err := http.NewRequest("GET", "/supers", nil)
+		if err != nil {
+			t.Errorf("This is the error: %v\n", err)
+		}
+		req = mux.SetURLVars(req, map[string]string{"uuid": v.uuid})
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.DeleteSuper)
+
+		handler.ServeHTTP(rr, req)
+		assert.Equal(t, rr.Code, v.statusCode)
+
+		if v.statusCode == 401 && v.errorMessage != "" {
+			responseMap := make(map[string]interface{})
+			err = json.Unmarshal([]byte(rr.Body.String()), &responseMap)
+			if err != nil {
+				t.Errorf("Cannot convert to json: %v", err)
+			}
+			assert.Equal(t, responseMap["error"], v.errorMessage)
+		}
+	}
+}
